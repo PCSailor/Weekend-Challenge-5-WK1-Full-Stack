03fab21d4d7d7151f1a799b3d34ccc98c30f1d25
Phil Curtis <Phil@PhilCurtis.io>
2017-03-12 12:45:16 -0500

    shit back working

--- public/scripts/controllers/task.controller.js
+++ public/scripts/controllers/task.controller.js
@@ -1,23 +1,18 @@
-// <<<<<<< HEAD
 myApp.controller('EmployeeController', ['$http', 'EmployeeFactory', function($http, EmployeeFactory){
   console.log('EmployeeController is loaded');
-// =======
-// myApp.controller('TaskController', ['$http', 'EmployeeFactory', function($http, EmployeeFactory){
-//   console.log('The TaskController was loaded');
-// >>>>>>> parent of d4d1dcf... adding functionality-buttons working
   var self = this;
-  self.newTask = {};
+  self.newEmployee = {};
   self.taskList = EmployeeFactory.allEmployees;
 
   self.addEmployee = function() {
     $http({
       method: 'POST',
       url: '/employee',
-      data: self.newTask
+      data: self.newEmployee
     }).then(function(response){
       console.log(response);
       EmployeeFactory.updateEmployees();
-      self.newTask = {};
+      self.newEmployee = {};
     });
   }
 
@@ -42,11 +37,7 @@ myApp.controller('EmployeeController', ['$http', 'EmployeeFactory', function($ht
   self.inactiveEmployee = function(employeeId) {
     $http({
       method: 'PUT',
-// <<<<<<< HEAD
       url: '/employee/inactive/' + employeeId  // TODO:
-// =======
-//       url: '/employee/uncomplete/' + taskId  // TODO:
-// >>>>>>> parent of d4d1dcf... adding functionality-buttons working
     }).then(function(response) {
       EmployeeFactory.updateEmployees();
     });
